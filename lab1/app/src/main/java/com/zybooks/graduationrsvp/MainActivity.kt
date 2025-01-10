ce365c2af78c706e466ca08502ccccdcdff36ec1
BrentNakashima <brentnakashima@Brents-MacBook-Air-4.local>
2025-01-10 10:27:16 -0800

    Added grad cap photo to screen

--- lab1/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ lab1/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -3,12 +3,18 @@ package com.zybooks.graduationrsvp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.zybooks.graduationrsvp.ui.theme.GraduationRSVPTheme
@@ -32,9 +38,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
     Text(
-        text = "Graduation Announcement",
+        text = stringResource(R.string.graduation_announcement),
         fontSize = 50.sp,
+        textAlign = TextAlign.Center,
+        lineHeight = 50.sp,
+        color = Color.Red,
+        fontWeight = FontWeight.Bold,
         modifier = modifier
+
+    )
+    Image(
+        painter = painterResource(R.drawable.grad_cap),
+        contentDescription = "Graduation cap",
+        alpha = 0.3f
     )
 }
 
